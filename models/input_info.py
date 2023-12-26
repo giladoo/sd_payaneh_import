8bb6c50dd17aea0ebc60c63c69e70331741524a3
arash <arash@Arash.arash>
2023-12-26 05:05:13 -0500

    update 1.1.5

--- models/input_info.py
+++ models/input_info.py
@@ -16,6 +16,24 @@ class SdPayanehNaftiInputInfoAmount(models.Model):
 
     remain_amount_old = fields.Float()
 
+    def make_done(self):
+        active_ids = self.env.context.get('active_ids')
+        # print(f'--------> active_ids: {active_ids}')
+        selected = self.browse(active_ids)
+        new_data = {}
+        for rec in selected:
+            if rec.state != 'done':
+                new_data['state'] = 'done'
+                rec.write(new_data)
+    def update_loading_info_date(self):
+        active_ids = self.env.context.get('active_ids')
+        # print(f'--------> active_ids: {active_ids}')
+        selected = self.browse(active_ids)
+        new_data = {}
+        for rec in selected:
+            new_data['loading_info_date'] = rec.loading_date
+            rec.write(new_data)
+
 
 
 
